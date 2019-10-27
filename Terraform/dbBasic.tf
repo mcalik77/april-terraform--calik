a6c444fbbe77170a4783b03e878936c685451476
mcalik77 <cactus@cactuss-MacBook-Pro-2.local>
2019-10-26 21:22:23 -0700

    add basic appDB

---
+++ Terraform/dbBasic.tf
@@ -0,0 +1,15 @@
+resource "aws_db_instance" "appDB" {
+  
+  vpc_security_group_ids   = ["${aws_security_group.appDB.id}"]
+  allocated_storage        = 20
+  storage_type             = "gp2"
+  engine                   = "postgres"
+  engine_version           = "9.5.4"
+  instance_class           = "db.t2.micro"
+  identifier               = "appDB"
+  name                     = "mydb"
+  username                 = "dbUser"
+  password                 = "password"
+  port                     = 5432
+
+}
