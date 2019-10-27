329062a7819114cc529a3710101b90acad58852c
mcalik77 <cactus@cactuss-MacBook-Pro-2.local>
2019-10-27 09:42:08 -0700

    edited

--- Terraform/instance.tf
+++ Terraform/instance.tf
@@ -16,7 +16,7 @@ resource "aws_instance" "appServer" {
     ami = "ami-08d489468314a58df"
     instance_type = "t2.micro"
     #If you create a keypair and you assign to instance if you want to 
-    #key_name = "${aws_key_pair.terraform_april.key_name}"
+    key_name = "${var.key_name}"
     security_groups = ["${aws_security_group.public.name}"]
     user_data = "${file("userdata-appServer.sh")}"
 
