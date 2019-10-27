c7ca8445cef1082fff622087a57395086a23c228
mcalik77 <cactus@cactuss-MacBook-Pro-2.local>
2019-10-27 09:44:16 -0700

    edited

--- Terraform/instance.tf
+++ Terraform/instance.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "webServer" {
     ami = "ami-08d489468314a58df"
     instance_type = "t2.micro"
-    #key_name = "${aws_key_pair.terraform_april.key_name}"
-    security_groups = ["${aws_security_group.public.name}"]
+    key_name = "${var.key_name}"
+    #security_groups = ["${aws_security_group.public}"]
     user_data = "${file("userdata-webServer.sh")}"
 
     tags = {
@@ -17,7 +17,7 @@ resource "aws_instance" "appServer" {
     instance_type = "t2.micro"
     #If you create a keypair and you assign to instance if you want to 
     key_name = "${var.key_name}"
-    security_groups = ["${aws_security_group.public.name}"]
+    #security_groups = ["${aws_security_group.public}"]
     user_data = "${file("userdata-appServer.sh")}"
 
     tags = {
